7df61aed43e5a6a2909d1b10e2930033ab66c52a
Folgue02 <davidfolru@gmail.com>
2022-06-12 12:28:48 +0200

    Python client for testing purposes

---
+++ client.py
@@ -0,0 +1,33 @@
+from threading import Thread 
+import socket 
+
+CONN = ("127.0.0.1", 8080)
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(CONN)
+
+    Thread(target=sender, args=[server]).start()
+
+
+    count = 0
+    print("Listening")
+    while True:
+        msg = server.recv(1024)
+        print(f"Message received {count}: {msg.decode('utf-8')}")
+        count += 1
+
+
+
+
+def sender(sock):
+    while True:
+        ui = input("SEND ME? ")
+        
+
+        ui = f"type=msg:msg={ui}"
+        print(f"Sending message {ui} to user")
+        sock.send(ui.encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
